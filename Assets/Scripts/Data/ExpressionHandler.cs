02e8e09f9ce19afc0f39c8842526942e5f741241
CaitaXD <caitaXD@gmail.com>
2021-12-22 18:12:53 -0300

    Refactoring of The Expression Hanbdler

--- Assets/Scripts/Data/ExpressionHandler.cs
+++ Assets/Scripts/Data/ExpressionHandler.cs
@@ -11,6 +11,8 @@ public class ExpressionHandler
         {"+",2},{"-",2},{"d",3},{"*",1},{"/",1}
     };
     System.Random rand = new System.Random(Mathf.RoundToInt(System.DateTime.Now.Millisecond)); //PLACEHOLDER
+
+    // Breaks the command string into to varius operation, checking operation precedence
     public List<float> HandleExpression(string argument)
     {
         _argument = argument;
@@ -32,77 +34,89 @@ public class ExpressionHandler
         float A = StringToNumber(operandA);
         float B = StringToNumber(operandB);
 
-        void ParseNANoperators(float value) //StrinToNumber will return NAN if the string is empty
+        //Neutral Member of Group, Varies by operation
+        void SetNeutralMemberValue(float value) 
         {
-            A = AssginValueIfNAN(A, value); 
-            B = AssginValueIfNAN(B, value);
+            A = AssignIfNeutralMember(A, value); 
+            B = AssignIfNeutralMember(B, value);
         }
         string currentOperation;
         float newOperation;
         int leanghtOfA = A.ToString().Length;
         int leanghtOfB = B.ToString().Length;
+
         switch (Operator)
         {
             case "+":
-                ParseNANoperators(0);
+                SetNeutralMemberValue(0);
                 currentOperation = (A + B).ToString();
-                newOperation = HandleExpression(GetNewOperationLeftOfCurrent(argument, leanghtOfA, IndexesOfOperations) + currentOperation + GetNewOperationRightOfCurrent(argument, leanghtOfB, IndexesOfOperations))[0];
+                newOperation = HandleExpression(GetNewLeftOperation(argument, leanghtOfA, IndexesOfOperations) + currentOperation + GetNewRightOperation(argument, leanghtOfB, IndexesOfOperations))[0];
                 Results.Add(newOperation);
                 return Results;
 
             case "-":
-                ParseNANoperators(0);
+                SetNeutralMemberValue(0);
                 currentOperation = (A - B).ToString();
-                newOperation = HandleExpression(GetNewOperationLeftOfCurrent(argument, leanghtOfA, IndexesOfOperations) + currentOperation + GetNewOperationRightOfCurrent(argument, leanghtOfB, IndexesOfOperations))[0];
+                newOperation = HandleExpression(GetNewLeftOperation(argument, leanghtOfA, IndexesOfOperations) + currentOperation + GetNewRightOperation(argument, leanghtOfB, IndexesOfOperations))[0];
                 Results.Add(newOperation);
                 return Results;
 
             case "*":
-                ParseNANoperators(1);
+                SetNeutralMemberValue(1);
                 currentOperation = (A * B).ToString();
-                newOperation = HandleExpression(GetNewOperationLeftOfCurrent(argument, leanghtOfA, IndexesOfOperations) + currentOperation + GetNewOperationRightOfCurrent(argument, leanghtOfB, IndexesOfOperations))[0];
+                newOperation = HandleExpression(GetNewLeftOperation(argument, leanghtOfA, IndexesOfOperations) + currentOperation + GetNewRightOperation(argument, leanghtOfB, IndexesOfOperations))[0];
                 Results.Add(newOperation);
                 return Results;
 
             case "/":
-                ParseNANoperators(1);
+                SetNeutralMemberValue(1);
                 if(B == 0)
                 { 
                     Results.Add(float.NaN);
                     return Results;
                 }
                 currentOperation = (A / B).ToString();
-                newOperation = HandleExpression(GetNewOperationLeftOfCurrent(argument, leanghtOfA, IndexesOfOperations) + currentOperation + GetNewOperationRightOfCurrent(argument, leanghtOfB, IndexesOfOperations))[0];
+                newOperation = HandleExpression(GetNewLeftOperation(argument, leanghtOfA, IndexesOfOperations) + currentOperation + GetNewRightOperation(argument, leanghtOfB, IndexesOfOperations))[0];
                 Results.Add(newOperation);
                 return Results;
 
             case "d":
-                ParseNANoperators(1);
-                for (int i = 0; i < A || i < 1; i++)
-                    Results.Add(rand.Next(1, (int)B + 1));
+                A = AssignIfNeutralMember(A, 1);
+                if (float.IsNaN(B)) return Results;
+                for (int i = 0; i < A || i < 1; i++) Results.Add(rand.Next(1, (int)B + 1));
                 return Results;
         }
         return new List<float>();
     }
-
-    private string GetNewOperationLeftOfCurrent(string argument, int lenght, List<int> IndexesOfOperations)
+    /*
+     *  Gets the new operation left of the current one, if one exists.
+     *  Else returns an empty string.
+     */
+    private string GetNewLeftOperation(string argument, int lenght, List<int> IndexesOfOperations)
     {
         if (IndexesOfOperations.Count > 1)
         {
             string beforeOperator = argument.Substring(0, IndexesOfOperations[0] - lenght);
                         return beforeOperator;
         }
-        return "";
+        return string.Empty;
     }
-    private string GetNewOperationRightOfCurrent(string argument, int lenght, List<int> IndexesOfOperations)
+    /*
+    *  Gets the new operation right of the current one, if one exists.
+    *  Else returns an empty string.
+    */
+    private string GetNewRightOperation(string argument, int lenght, List<int> IndexesOfOperations)
     {
         if (IndexesOfOperations.Count > 1)
         {
             string afterOperator = argument.Substring(IndexesOfOperations[0] + lenght + 1);
             return afterOperator;
         }
-        return "";
+        return string.Empty;
     }
+    /*
+    *  Searches for the indexes of all the operations within the string
+    */
     private List<int> GetIndexesOfOperations(string argument)
     {               
         List<int> opis = new List<int>();
@@ -127,6 +141,8 @@ public class ExpressionHandler
         if(opis.Count > 0)return opis;
         else return new List<int>(){0};
     }
+
+    //Extracts a number for an given string and NaN if theres none
     private float StringToNumber(string arg)
     {
         List<string> numbers = new List<string> { "1","2","3","4","5","6","7","8","9","0","-",",","."};
@@ -141,46 +157,73 @@ public class ExpressionHandler
         if (str == "") return float.NaN;
         return float.Parse(str);
     }
-
+    /*
+    *  Returns a substring with the number left of the current operation
+    */
     private string GetOperandA(int index,string argument)
+    {
+        string substring = argument.Substring(0, index);
+        char sign = substring.FirstOrDefault();
+        int operations = GetIndexesOfOperations(substring).Count;
+
+        bool oneOp = OperandContainsAtMostOneOperation(substring, sign, operations);
+        if (oneOp) return substring;
+
+        // Finds the index where the current operation ends
+        bool check = true;
+        while (check)
         {
-            string sub = argument.Substring(0,index);
-            char sign = sub.FirstOrDefault();
-            bool check = true;
-            int operations = GetIndexesOfOperations(sub).Count;
-            bool firstDigitisSign = Operations.ContainsKey(sign.ToString());
-            if(operations <= 1 && (!firstDigitisSign && GetIndexesOfOperations(sub)[0] == 0)) return sub;
-            while(check)
-            {
-                index--;
-                check = !Operations.Keys.Any(x => x == sub[index].ToString());
-                if(check == false)index++;
-            }
-            if(Operations.ContainsKey(sign.ToString())) sub = sign + sub.Substring(index);
-            else sub = sub.Substring(index);
-            return sub;
+            index--;
+            check = !Operations.Keys.Any(x => x == substring[index].ToString());
+            if(check == false)index++;
         }
-        private string GetOperandB(int index,string argument)
+        if(Operations.ContainsKey(sign.ToString())) substring = sign + substring.Substring(index);
+        else substring = substring.Substring(index);
+        return substring;
+    }
+    /*
+    *  Returns a substring with the number right of the current operation
+    */
+    private string GetOperandB(int index, string argument)
+    {
+        string substring = argument.Substring(index + 1);
+        if (substring.Length == 0) substring = " ";
+        char sign;
+        sign = substring[0];
+        int operations = GetIndexesOfOperations(substring).Count;
+
+        bool oneOp = OperandContainsAtMostOneOperation(substring, sign, operations);
+        if (oneOp) return substring;
+
+        // Finds the index where the current operation ends
+        bool check = true;
+        index = 0;
+        while (check)
         {
-            string sub = argument.Substring(index + 1);
-            char sign = sub[0];
-            int operations = GetIndexesOfOperations(sub).Count;
-            bool firstDigitisSign = Operations.ContainsKey(sign.ToString());
-            if(operations <= 1 && (!firstDigitisSign && GetIndexesOfOperations(sub)[0] == 0)) return sub;
-            bool check = true;
-            index = 0;
-            while(check)
-            {
-                check = !Operations.Keys.Any(x => x == sub[index].ToString());
-                index++;
-            }
-            if(index <= sub.Length)sub = sub.Substring(0, index);
-            if(Operations.ContainsKey(sub[sub.Length - 1].ToString())) sub = sub.Substring(0, sub.Length - 1);
-            return sub;
+            check = !Operations.Keys.Any(x => x == substring[index].ToString());
+            index++;
         }
-        private void SortIndexesByOrderOfOperations(List<int> OperationsIndexes, string argument)
+        if (index <= substring.Length) substring = substring.Substring(0, index);
+        if (Operations.ContainsKey(substring[substring.Length - 1].ToString())) substring = substring.Substring(0, substring.Length - 1);
+        return substring;
+    }
+    /*
+    *  Returns true if the given substring contains at most one operation, expect in the cases were the first digit represents a sign
+    */
+    private bool OperandContainsAtMostOneOperation(string substring, char sign, int operations)
+    {
+        bool firstDigitisSign = Operations.ContainsKey(sign.ToString());
+        bool firstDigitIsOperation = (!firstDigitisSign && GetIndexesOfOperations(substring)[0] == 0);
+        bool operandContainsAtMostOneOperation = operations <= 1 && firstDigitIsOperation;
+        return operandContainsAtMostOneOperation;
+    }
+
+    /*
+    *  Given a list of indexs of the operations of the given string, sorts it by said order of operations
+    */
+    private void SortIndexesByOrderOfOperations(List<int> OperationsIndexes, string argument)
         {
-            int aux = 999;
+            int aux = int.MaxValue;
             for (int i = 0; i < OperationsIndexes.Count; i++)
             {
                 int op = OperationsIndexes[i];
@@ -193,9 +236,11 @@ public class ExpressionHandler
                 }
             }
         }
-        private float AssginValueIfNAN(float shoringerNumber,float value)
+
+        // Assign the value of the Neutral memeber, NaN is representing the neutral Member
+        private float AssignIfNeutralMember(float shoringerNumber,float value)
         {
-           if(float.IsNaN(shoringerNumber)) return value; //if is not a number raturn a number
-            else return shoringerNumber; //if its is a number return it
+           if(float.IsNaN(shoringerNumber)) return value;
+            else return shoringerNumber;
         }
 }
